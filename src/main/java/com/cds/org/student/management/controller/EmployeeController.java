27a61e6d5bcda25cccc5f9cc6271857cf5e24ece
Chandrakant <csolanke77@gmail.com>
2021-11-28 21:35:31 -0500

    Code changes to handle CORS policy issue while making backend call from UI

--- src/main/java/com/cds/org/student/management/controller/EmployeeController.java
+++ src/main/java/com/cds/org/student/management/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.cds.org.student.management.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cds.org.student.management.system.model.Employee;
 import com.cds.org.student.management.system.repository.EmployeeRepository;
 
+
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("api/v1/")
 public class EmployeeController {
