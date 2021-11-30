21a33af550cbc489a446022d710bf0308963c61e
Chandrakant <csolanke77@gmail.com>
2021-11-29 19:43:38 -0500

    Added new method to sms api to save a new employee

--- src/main/java/com/cds/org/student/management/controller/EmployeeController.java
+++ src/main/java/com/cds/org/student/management/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,13 @@ public class EmployeeController {
 	{
 		return repository.findAll();
 	}
+	
+	//Add new Employee
+	
+	@PostMapping("/employees")
+   public Employee addEmployee(@RequestBody Employee employee) {
+		return repository.save(employee);
+			
+		}
 
 }
