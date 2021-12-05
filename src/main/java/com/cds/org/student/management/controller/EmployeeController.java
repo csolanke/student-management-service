51ec21392c726d6c36dd5e059f5adb1a3c1e5a50
Chandrakant <csolanke77@gmail.com>
2021-12-05 16:20:56 -0500

    Added new methods for employee fetch and updates

--- src/main/java/com/cds/org/student/management/controller/EmployeeController.java
+++ src/main/java/com/cds/org/student/management/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.cds.org.student.management.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cds.org.student.management.system.exception.ResourceNotFoundException;
 import com.cds.org.student.management.system.model.Employee;
 import com.cds.org.student.management.system.repository.EmployeeRepository;
 
@@ -37,5 +41,28 @@ public class EmployeeController {
 		return repository.save(employee);
 			
 		}
+	
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployee(@PathVariable Long id)
+	{
+		Employee employee = repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee with requested not present"));
+		return ResponseEntity.ok(employee);
+		
+	}
+	
+	
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployees(@PathVariable Long id , @RequestBody Employee employee)
+	{
+		Employee emp = repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee with requested not present"));
+	
+		emp.setFirstName(employee.getFirstName());
+		emp.setLastName(employee.getLastName());
+		emp.setEmailId(employee.getEmailId());
+		
+		return ResponseEntity.ok(emp);
+	}
+	
 
 }
