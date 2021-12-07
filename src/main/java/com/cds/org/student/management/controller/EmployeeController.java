73ad9672bc28ca7ca8d43e721af29e4803408e68
Chandrakant <csolanke77@gmail.com>
2021-12-06 20:33:32 -0500

    Added Delete method in employee controller to perform delete operation on employee entity

--- src/main/java/com/cds/org/student/management/controller/EmployeeController.java
+++ src/main/java/com/cds/org/student/management/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.cds.org.student.management.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +52,19 @@ public class EmployeeController {
 		Employee employee = repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee with requested not present"));
 		return ResponseEntity.ok(employee);
 		
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id)
+	{
+		Employee employee = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Requested resource is not available for deletion"));
+		repository.delete(employee);
+
+		Map<String, Boolean> response = new HashMap<String, Boolean>();
+		response.put("Deleted", Boolean.TRUE);
+
+		return ResponseEntity.ok(response);
 	}
 	
 	
